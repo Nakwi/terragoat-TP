d5e0493be2f3b2e695dd60ca3fecf3c122bca2d4
Nakwi <corsyn.ryan04@gmail.com>
2026-06-25 14:28:36 +0000

    Fix: location Azure valide + vraie cle SSH publique (corrige valeurs bidon TerraGoat)

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -23,5 +23,5 @@ variable "admin_ip" {
 variable "admin_ssh_public_key" {
   type        = string
   description = "Cle SSH publique pour l'acces a la VM Linux"
-  default     = "ssh-rsa AAAAB3NzaC1yc2EXAMPLEKEYxxxxxxxxxxxxxxxx admin@terragoat"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9QuwG2vXgeM9vEKwFUaZa4UtLw3l6S2KpvIT85OBGV9bFBr0g7NMHzd8nIypGuhkyhNVVyv/te16rpUEyss2lfcj82/US249Ugp7o4uD0uC5/zZdy7cz90ZURu6fgSW2/kQMhPsCiBrH0sVMjzwUqM7a/bdnrNZjHnb3zbuFkr2WctlX2J0yiRnVioZzKLsZNb4sNoXiAmoqnoNKrkGTj0P3p+SkLDpRIo2e93qKHCaCxE2MS1qBAbf0HHqrbETaI88UfYEZiJzS1YeIWwbh7NN8MSzpGMyeVulxJ6TP+1iYnZML7sMO9vq8pE90vS8KvrXYXUPvJKEDcHUTbq+y5l1X8+knbgExobMSknuTSTmiQvcJbUNnlblsvZ4Nmw9PU/gEYj+KF3B1yXbdQ8va4qzl2Lcxk3AGctiN8sjyfE1jTXXCKBmivUPsGol8ocSY8jQGl51qBkKTMM4X6HDmqd51FitiEfH6N30/JA0I4fLguNdfuiydsF89+3t8ieULNNruUanqtiYv0+otODiKc9qrFtZf1xMV4Qzbf90WcK8Ajm6Mk4yrWt4WZ9VzAmXJGj2UlgzdVo4bxSS51Hx3zoHZgI2/sxXpaIJtrHePU3535x3xvgD6pD923Axa80ko2y+ULEZjyDwa2oWhwhwo2atFi+B1xHpz4PcR+Hrlqaw== azureuser@vm-terragoat"
 }
